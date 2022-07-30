87d572fb7cdc304bb0a96df39079388414289a61
Your Name <youremail@yourdomain.com>
2022-07-30 10:15:43 -0700

    classLIst & createElement

--- js/javascript.js
+++ js/javascript.js
@@ -10,7 +10,25 @@ async function loadMovies(searchTerm) {
 	const URL =  `http://www.omdbapi.com/?s=${searchTerm}&page=1&apikey=546c2681`;
 	const res = await fetch(`${URL}`);
 	const data = await res.json();
-	console.log(data);
+	//console.log(data.Search);
+	if(data.Response =='True')
+	displayMovieList(data.Search);
 }
 
-loadMovies('lord of the rings');
+function findMovies() {
+	let searchTerm = (movieSearchBox.value).trim();
+	if(searchTerm) {
+		searchList.classList.remove('hide-search-list');
+		loadMovies(searchTerm);
+	} else {
+		searchList.addEventListener('hide-search-list');
+	}
+}
+
+function displayMovieList(movies) {
+	searchList.innerHTML = '';
+	for(let idx = 0; idx < movies.length; idx++) {
+		let movieListItem = document.createElement('div');
+		console.log(movieListItem);
+	}
+}
