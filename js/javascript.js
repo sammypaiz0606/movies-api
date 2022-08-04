92c1712b580927bfb5c7fb8986bdac8f552628a7
Your Name <youremail@yourdomain.com>
2022-08-04 15:20:03 -0700

    testing the api key

--- js/javascript.js
+++ js/javascript.js
@@ -57,7 +57,7 @@ function loadMovieDetails(){
             // console.log(movie.dataset.id);
             searchList.classList.add('hide-search-list');
             movieSearchBox.value = "";
-            const result = await fetch(`http://www.omdbapi.com/?i=${movie.dataset.id}&apikey=546c2681`);
+            const result = await fetch(`https://www.omdbapi.com/?i=${movie.dataset.id}&apikey=546c2681`);
             const movieDetails = await result.json();
             // console.log(movieDetails);
             displayMovieDetails(movieDetails);
